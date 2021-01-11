fabb70da2b8e0a44b85fb7404a4a28ed96455c54
Sanblch <aleksey1shishkin@yandex.ru>
2021-01-12 00:33:46 +0300

    func drawField

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ let secondPlayerCount = document.getElementById("secondResult")
 let currentTurn = document.getElementById("turn");
 const winX = ["X", "X", "X"];
 const win0 = ["0", "0", "0"];
+
+function drawField() 
+{
+	let canvas = document.getElementById("ticTacToe");
+    ctx = canvas.getContext("2d");
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    ctx.font = "250pt Arial";
+    ctx.lineWidth = 40;
+    ctx.beginPath()
+    ctx.moveTo(canvas.width/3, 0); 
+    ctx.lineTo(canvas.width/3, canvas.height); 
+    ctx.moveTo(canvas.width/3*2, 0); 
+    ctx.lineTo(canvas.width/3*2, canvas.height); 
+    ctx.moveTo(0, canvas.height/3); 
+    ctx.lineTo(canvas.width, canvas.height/3);
+    ctx.moveTo(0, canvas.height/3*2); 
+    ctx.lineTo(canvas.width, canvas.height/3*2); 
+    ctx.stroke();
+}
